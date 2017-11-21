f43c2eaefdd017a8733be4ba3dd4ee28b79c106d
Jeff Gan <jeff@tenx.tech>
2017-11-21 15:07:29 +0800

    Libraries and unit test files

---
+++ app/src/main/java/com/example/jeffgan/mvvm_example/SharedPreferenceEntry.java
@@ -0,0 +1,40 @@
+package com.example.jeffgan.mvvm_example;
+
+/**
+ * Created by jeffgan on 21/11/17.
+ */
+
+import java.util.Calendar;
+
+/**
+ * Model class containing personal information that will be saved to SharedPreferences.
+ */
+public class SharedPreferenceEntry {
+
+    // Name of the user.
+    private final String mName;
+
+    // Date of Birth of the user.
+    private final Calendar mDateOfBirth;
+
+    // Email address of the user.
+    private final String mEmail;
+
+    public SharedPreferenceEntry(String name, Calendar dateOfBirth, String email) {
+        mName = name;
+        mDateOfBirth = dateOfBirth;
+        mEmail = email;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public Calendar getDateOfBirth() {
+        return mDateOfBirth;
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+}
